9ba3b8a5df48bb7687eccb160ab109d5cd8db137
Timm Friebe <thekid@thekid.de>
2015-01-25 17:00:39 +0100

    Add support for tolerating previous and next tokens

---
+++ src/main/php/com/google/authenticator/Tolerance.class.php
@@ -0,0 +1,28 @@
+<?php namespace com\google\authenticator;
+
+class Tolerance extends \lang\Object {
+  public static $NONE, $PREVIOUS_AND_NEXT;
+  private $past, $future;
+
+  static function __static() {
+    self::$NONE= new self(0, 0);
+    self::$PREVIOUS_AND_NEXT= new self(-1, +1);
+  }
+
+  /**
+   * Creates a new tolerance instance
+   *
+   * @param  int $past How many previous tokens to allow (< 0)
+   * @param  int $future How many next tokens to allow (> 0)
+   */
+  public function __construct($past, $future) {
+    $this->past= $past > 0 ? -$past : $past;
+    $this->future= $future < 0 ? -$future : $future;
+  }
+
+  /** @return int */
+  public function past() { return $this->past; }
+
+  /** @return int */
+  public function future() { return $this->future; }
+}

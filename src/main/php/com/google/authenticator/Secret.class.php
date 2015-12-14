e077539a435d2829372b76b53beec7a75d9e3033
Timm Friebe <thekid@thekid.de>
2015-12-14 15:03:38 +0100

    Deprecate Secret::random() - replaced by Secrets::random() in utility class

--- src/main/php/com/google/authenticator/Secret.class.php
+++ src/main/php/com/google/authenticator/Secret.class.php
@@ -3,8 +3,6 @@
 use lang\IllegalStateException;
 
 class Secret extends \lang\Object {
-  const LENGTH = 10;
-
   private $bytes;
 
   /**
@@ -81,22 +79,8 @@ class Secret extends \lang\Object {
    * Returns a new random secret. Uses the OpenSSL and MCrypt libraries
    * if available, falling back to the system random number generator.
    *
-   * @see    php://openssl_random_pseudo_bytes
-   * @see    php://mcrypt_create_iv
-   * @see    php://rand
-   * @return com.google.authenticator.Secret
+   * @deprecated Use Secrets::random() instead
+   * @return self
    */
-  public static function random() {
-    if (function_exists('openssl_random_pseudo_bytes')) {
-      $bytes= openssl_random_pseudo_bytes(self::LENGTH);
-    } else if (function_exists('mcrypt_create_iv')) {
-      $bytes= mcrypt_create_iv(self::LENGTH, MCRYPT_DEV_RANDOM);
-    } else {
-      $bytes= '';
-      for ($i= 0; $i < self::LENGTH; $i++) {
-        $bytes.= chr(rand(0, 255));
-      }
-    }
-    return new self($bytes);
-  }
+  public static function random() { return Secrets::random(); }
 }
